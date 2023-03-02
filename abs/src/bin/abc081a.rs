5dfb4bb8d87c953ffca37dff7f38817595e96d48
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2023-03-02 14:09:42 +0900

    提出：abc081a

--- abs/src/bin/abc081a.rs
+++ abs/src/bin/abc081a.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+use proconio::marker::Chars;
 fn main() {
-    todo!();
+    input! {
+        s: Chars,
+    }
+    let mut ans = 0;
+    for si in s {
+        if si.to_digit(10).unwrap() == 1 {
+            ans = ans + 1;
+        }
+    }
+    println!("{}", ans);
 }
