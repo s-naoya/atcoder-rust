60bfa6c961182008132f951706716f2376bd746b
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2023-03-02 14:01:04 +0900

    提出：abc086a

--- abs/src/bin/abc086a.rs
+++ abs/src/bin/abc086a.rs
@@ -1,3 +1,12 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        ab: [i32; 2]
+    }
+    if ab[0]*ab[1] % 2 == 0 {
+        println!("Even");
+    } else {
+        println!("Odd");
+    }
 }
