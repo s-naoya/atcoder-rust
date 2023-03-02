3b0779bac9aeccb4d7317b4731ed0319b1388f84
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2023-03-02 13:51:42 +0900

    提出：abs-practice-a

--- abs/src/bin/practice-a.rs
+++ abs/src/bin/practice-a.rs
@@ -1,3 +1,12 @@
+use proconio::marker::Chars;
+use proconio::input;
+use std::iter::FromIterator;
+
 fn main() {
-    todo!();
+    input! {
+        a: i32,
+        bc: [i32; 2],
+        s: Chars,
+    }
+    println!("{} {}", a+bc[0]+bc[1], String::from_iter(s));
 }
