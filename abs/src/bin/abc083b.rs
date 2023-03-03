b2334ed5326412c9641f74d09c2efa15795a56da
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2023-03-03 22:59:23 +0900

    提出：abc083b

--- abs/src/bin/abc083b.rs
+++ abs/src/bin/abc083b.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        nab: [u32; 3],
+    }
+    let mut ans: u32 = 0;
+    for n in 0..nab[0]+1 {
+        let ns = n.to_string();
+        let mut nsum: u32 = 0;
+        for ni in ns.chars() {
+            nsum = nsum + ni.to_digit(10).unwrap();
+        }
+        if nsum >= nab[1] && nsum <= nab[2] {
+            ans = ans + n;
+        }
+    }
+    println!("{}", ans);
 }
