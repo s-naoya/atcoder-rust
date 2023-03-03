8eb5cc7737cbf09195faa7539c6505427d6e3e1b
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2023-03-03 22:59:58 +0900

    提出：abc085c

--- abs/src/bin/abc085c.rs
+++ abs/src/bin/abc085c.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        ny: [i32; 2],
+    }
+
+    for x in 0..=ny[0] {
+        for y in 0..=ny[0]-x {
+            let z = ny[0] - x - y;
+            if 10000*x + 5000*y + 1000*z == ny[1] {
+                println!("{} {} {}", x, y, z);
+                return
+            }
+        }
+    }
+    println!("-1 -1 -1");
 }
+// 34:31.74
