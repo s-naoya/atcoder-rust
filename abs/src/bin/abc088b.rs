e2bf8fa97d724155e713b017808cbe721aae9f22
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2023-03-03 22:59:35 +0900

    提出：abc088b

--- abs/src/bin/abc088b.rs
+++ abs/src/bin/abc088b.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        mut a: [i32; n],
+    }
+    a.sort();
+    let (mut alice, mut bob) = (0, 0);
+    loop {
+        alice = alice + a.pop().unwrap();
+        if a.len() == 0 {
+            break;
+        }
+        bob = bob + a.pop().unwrap();
+        if a.len() == 0 {
+            break;
+        }
+    }
+    println!("{}", alice - bob);
 }
+// 09:02.97
