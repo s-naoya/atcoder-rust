a9761d3e3a61ab0ad33dd66f5a379382eec8cf34
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2023-03-03 22:59:10 +0900

    提出：abc087b

--- abs/src/bin/abc087b.rs
+++ abs/src/bin/abc087b.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: i32,
+        b: i32,
+        c: i32,
+        x: i32,
+    }
+    let mut ans = 0;
+    for ai in 0..a+1 {
+        for bi in 0..b+1 {
+            for ci in 0..c+1 {
+                if 500*ai + 100*bi + 50*ci == x {
+                    ans = ans + 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
