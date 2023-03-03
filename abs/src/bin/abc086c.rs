aefc755b10ccaf677db1876a32fae95a0c58d8c5
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2023-03-03 23:00:21 +0900

    提出：abc086c

--- abs/src/bin/abc086c.rs
+++ abs/src/bin/abc086c.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        t: [[i32; 3]; n],
+    }
+    let mut ti1= vec![0, 0, 0];
+    for ti in t {
+        let kyori = (ti[1]-ti1[1]).abs() + (ti[2]-ti1[2]).abs();
+        let kyori_t = kyori - (ti[0]-ti1[0]);
+        if kyori_t == 0 {
+            ti1 = ti.clone();
+            continue;
+        } else if kyori < ti[0] - ti1[0] && kyori_t % 2 == 0 {
+            ti1 = ti.clone();
+            continue;
+        } else {
+            println!("No");
+            return;
+        }
+    }
+    println!("Yes");
 }
