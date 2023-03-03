67b8e19bb5207738829e4a984ac24e83b0c9cf81
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2023-03-03 22:59:46 +0900

    提出：abc085b

--- abs/src/bin/abc085b.rs
+++ abs/src/bin/abc085b.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        mut d: [i32; n],
+    }
+    d.sort();
+    let mut ans = 0;
+    let mut before = 0;
+    for di in d {
+        if di != before {
+            ans += 1;
+        }
+        before = di;
+    }
+
+    println!("{}", ans);
 }
+// 10:44.37
