6f0b165324628fdb6b3eddf20350eb830e9239d9
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2023-03-04 13:50:21 +0900

    提出：typical90-010

--- typical90/src/bin/010.rs
+++ typical90/src/bin/010.rs
@@ -1,4 +1,35 @@
 use proconio::input;
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        cp: [[usize; 2]; n],
+        q: usize,
+        lr: [[usize; 2]; q],
+    }
+    let (mut note_1, mut note_2) = (vec![0; n+1], vec![0; n+1]);
+    for i in 1..=n {
+        if cp[i-1][0] == 1 {
+            note_1[i] = note_1[i-1] + cp[i-1][1];
+            note_2[i] = note_2[i-1];
+        } else {
+            note_1[i] = note_1[i-1];
+            note_2[i] = note_2[i-1] + cp[i-1][1];
+        }
+    }
+    // println!("{:?} {:?}", note_1, note_2);
+    for lri in lr {
+        println!("{} {}", note_1[lri[1]]-note_1[lri[0]-1], note_2[lri[1]]-note_2[lri[0]-1]);
+    }
+
+    // for lri in lr {
+    //     let (mut ans_1, mut ans_2) = (0, 0);
+    //     for i in lri[0]-1..lri[1] {
+    //         if *cp.get(i).unwrap().get(0).unwrap() == 1 as usize {
+    //             ans_1 += cp[i][1];
+    //         } else {
+    //             ans_2 += cp[i][1];
+    //         }
+    //     }
+    //     println!("{} {}", ans_1, ans_2);
+    // }
 }
