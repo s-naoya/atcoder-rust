a589861ec37cfceebdfb1d393b6bdf8540a9ea42
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2023-03-14 11:51:31 +0900

    3/14 二分探索

--- tessoku-book/src/bin/a14.rs
+++ tessoku-book/src/bin/a14.rs
@@ -1,3 +1,34 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        (n, k): (usize, usize),
+        a: [usize; n],
+        b: [usize; n],
+        c: [usize; n],
+        d: [usize; n],
+    }
+    let mut ab: Vec<usize> = Vec::new();
+    let mut cd: Vec<usize> = Vec::new();
+    for ai in &a {
+        for bi in &b {
+            ab.push(ai + bi);
+        }
+    }
+    for ci in &c {
+        for di in &d {
+            cd.push(ci + di);
+        }
+    }
+    cd.sort();
+    for abi in ab {
+        match cd.binary_search(&(k - abi)) {
+            Ok(_) => {
+                println!("Yes");
+                return;
+            }
+            Err(_) => continue,
+        }
+    }
+    println!("No");
 }
--- tessoku-book/src/bin/b14.rs
+++ tessoku-book/src/bin/b14.rs
@@ -1,3 +1,48 @@
+use proconio::input;
 fn main() {
-    todo!();
+    input! {
+        (n, k): (usize, usize),
+        a: [usize; n],
+    }
+    let a1 = Vec::from(&a[0..n / 2]);
+    let a2 = Vec::from(&a[n / 2..n]);
+    let mut b1 = Vec::new();
+    let mut b2 = Vec::new();
+    // bit全探索
+    // 0..1 << a1.len() = 1を左にn/2ビットシフトした数-1まで = 2^n-1
+    // ex n/2 = 3 のとき、0..0b1000 = 8-1
+    for bit in 0..(1 << a1.len()) {
+        let mut sum = 0;
+        // bitを2進数としたときのの各桁を取り出す
+        for i in 0..n {
+            let num = (bit / (1 << i)) % 2; // 1 or 0
+            if num == 1 {
+                // 1のとき、処理を行う
+                sum += a1[i];
+            }
+        }
+        b1.push(sum);
+    }
+    for bit in 0..(1 << a2.len()) {
+        let mut sum = 0;
+        // bitを2進数としたときのの各桁を取り出す
+        for i in 0..n {
+            let num = (bit / (1 << i)) % 2; // 1 or 0
+            if num == 1 {
+                sum += a2[i];
+            }
+        }
+        b2.push(sum);
+    }
+    b2.sort();
+    for b1i in b1 {
+        match b2.binary_search(&(k - b1i)) {
+            Ok(_) => {
+                println!("Yes");
+                return;
+            }
+            Err(_) => continue,
+        }
+    }
+    println!("No");
 }
