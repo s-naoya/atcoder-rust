23202e6733f73d7725a3f185ada06679c78977cb
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2023-03-20 13:35:03 +0900

    テンプレート変更を反映

--- tessoku-book/src/bin/a21.rs
+++ tessoku-book/src/bin/a21.rs
@@ -2,7 +2,19 @@ use proconio::input;
 fn main() {
     input! {
         n: usize,
-        pn: [(usize, usize); n],
+        mut pn: [(usize, usize); n],
     }
-    let mut dp = vec![vec![0; n]; n];
+    pn.insert(0, (0, 0));
+    let pn = pn;
+    let mut dp = vec![vec![0; n + 1]; n + 1];
+    for l in 1..=n {
+        for r in (1..=n).rev() {
+            let score1 = if l <= pn[l - 1].0 && pn[l - 1].0 <= r {
+                pn[l - 1].1
+            } else {
+                0
+            };
+            // let score2 = if l <= pn[r+1].0 && pn[r+1].0
+        }
+    }
 }
