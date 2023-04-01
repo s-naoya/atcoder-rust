a92fb41241c4b93ac0c97abd6797f4f21ab8f865
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2023-04-01 16:35:24 +0900

    追加：3/30分

--- tessoku-book/src/bin/a21.rs
+++ tessoku-book/src/bin/a21.rs
@@ -5,16 +5,36 @@ fn main() {
         mut pn: [(usize, usize); n],
     }
     pn.insert(0, (0, 0));
+    pn.push((0, 0));
     let pn = pn;
     let mut dp = vec![vec![0; n + 1]; n + 1];
-    for l in 1..=n {
-        for r in (1..=n).rev() {
+    for len in (0..=(n - 2)).rev() {
+        for l in 1..=(n - len) {
+            let r = l + len;
             let score1 = if l <= pn[l - 1].0 && pn[l - 1].0 <= r {
                 pn[l - 1].1
             } else {
                 0
             };
-            // let score2 = if l <= pn[r+1].0 && pn[r+1].0
+            let score2 = if l <= pn[r + 1].0 && pn[r + 1].0 <= r {
+                pn[r + 1].1
+            } else {
+                0
+            };
+
+            dp[l][r] = if l == 1 {
+                dp[l][r + 1] + score2
+            } else if r == n {
+                dp[l - 1][r] + score1
+            } else {
+                std::cmp::max(dp[l - 1][r] + score1, dp[l][r + 1] + score2)
+            }
         }
     }
+
+    let mut ans = 0;
+    for i in 1..=n {
+        ans = std::cmp::max(ans, dp[i][i]);
+    }
+    println!("{}", ans);
 }
--- tessoku-book/src/bin/a22.rs
+++ tessoku-book/src/bin/a22.rs
@@ -1,4 +1,18 @@
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        mut a: [usize; n-1],
+        mut b: [usize; n-1]
+    }
+    a.insert(0, 0);
+    b.insert(0, 0);
+    // 遷移できないiからdp[a[i]]に遷移することを防ぐため、初期値を小さい値にする
+    let mut dp = vec![std::i32::MIN; n + 1];
+    dp[1] = 0;
+    for i in 1..n {
+        dp[a[i]] = std::cmp::max(dp[a[i]], dp[i] + 100);
+        dp[b[i]] = std::cmp::max(dp[b[i]], dp[i] + 150);
+    }
+    println!("{}", dp[n]);
 }
