2fed2ac94fc3a5582819794e4af83596d78bc537
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2023-04-10 22:38:21 +0900

    追加：4/10分

--- tessoku-book/src/bin/a64.rs
+++ tessoku-book/src/bin/a64.rs
@@ -1,12 +1,49 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 use proconio::input;
 fn main() {
     input! {
         (n, m): (usize, usize),
     }
-    let mut g = vec![vec![]; m];
+    let mut g = vec![vec![]; n + 1];
     for _ in 0..m {
         input! {(a, b, c): (usize, usize, usize)}
         g[a].push((b, c));
         g[b].push((a, c));
+    }
+
+    let mut cur = vec![1_000_000_000; n + 1];
+    cur[1] = 0;
+    let mut q = BinaryHeap::new();
+    let mut kakutei = vec![false; n + 1];
+    q.push((Reverse(0), 1));
+
+    while !q.is_empty() {
+        // println!("====");
+        let (_, pos) = q.pop().unwrap();
+        // print!("POP {} -> ", pos);
+        if kakutei[pos] {
+            // println!("NG");
+            continue;
+        }
+        // println!("OK");
+        kakutei[pos] = true;
+        // println!("{:?}", g);
+        for (gn, gc) in &g[pos] {
+            if kakutei[*gn] {
+                continue;
+            }
+            // print!("{}: {} or {} + {} -> ", *gn, cur[*gn], cur[pos], *gc);
+            cur[*gn] = std::cmp::min(cur[*gn], cur[pos].clone() + *gc);
+            q.push((Reverse(cur[*gn].clone()), *gn));
+            // println!("{}", cur[*gn])
+        }
+    }
+    for i in 1..=n {
+        if cur[i] == 1_000_000_000 {
+            println!("-1");
+        } else {
+            println!("{}", cur[i]);
+        }
     }
 }
--- tessoku-book/src/bin/b64.rs
+++ tessoku-book/src/bin/b64.rs
@@ -1,4 +1,47 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
+use itertools::Itertools;
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        (n, m): (usize, usize),
+    }
+
+    let mut g = vec![vec![]; n + 1];
+    for _ in 0..m {
+        input! {(a, b, c): (usize, usize, usize)}
+        g[a].push((b, c));
+        g[b].push((a, c));
+    }
+
+    let mut cur = vec![1_000_000_000; n + 1];
+    cur[1] = 0;
+    let mut q = BinaryHeap::new();
+    q.push((Reverse(cur[1]), 1));
+    let mut kakutei = vec![false; n + 1];
+
+    while !q.is_empty() {
+        let (_, pos) = q.pop().unwrap();
+        if kakutei[pos] {
+            continue;
+        }
+        kakutei[pos] = true;
+        for (gn, gc) in &g[pos] {
+            cur[*gn] = std::cmp::min(cur[*gn], cur[pos] + *gc);
+            q.push((Reverse(cur[*gn]), *gn));
+        }
+    }
+
+    let mut now = n;
+    let mut route = vec![n];
+    while now != 1 {
+        for (gn, gc) in &g[now] {
+            if cur[now].wrapping_sub(cur[*gn]) == *gc {
+                now = *gn;
+                break;
+            }
+        }
+        route.push(now);
+    }
+    println!("{}", route.iter().rev().join(" "));
 }
