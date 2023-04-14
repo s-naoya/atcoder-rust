bb8f69d18b59b085a16b0b9920b7f6d376d5dab4
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2023-04-14 13:20:45 +0900

    追加：4/13分

--- tessoku-book/src/bin/a65.rs
+++ tessoku-book/src/bin/a65.rs
@@ -1,4 +1,22 @@
+use itertools::Itertools;
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        a: [usize; n-1]
+    }
+    let mut g = vec![vec![]; n + 1];
+    for (i, ai) in a.iter().enumerate() {
+        g[*ai].push(i + 2);
+    }
+    let mut dp = vec![0; n + 1];
+    for i in (1..=n).rev() {
+        dp[i] = g[i].len();
+        for j in &g[i] {
+            dp[i] += dp[*j];
+        }
+    }
+
+    dp.remove(0);
+    println!("{}", dp.iter().join(" "));
 }
--- tessoku-book/src/bin/b65.rs
+++ tessoku-book/src/bin/b65.rs
@@ -1,4 +1,35 @@
+use itertools::Itertools;
 use proconio::input;
+
+fn dfs(g: &Vec<Vec<usize>>, dp: &mut Vec<i32>, joushi: usize, buka: usize) -> i32 {
+    // println!(
+    //     "=== joushi: {}, buka: {}, g[buka]: {:?}",
+    //     joushi, buka, g[buka]
+    // );
+    let mut max = -1;
+    for buka_no_buka in &g[buka] {
+        if *buka_no_buka == joushi {
+            continue;
+        }
+        max = std::cmp::max(max, dfs(g, dp, buka, *buka_no_buka));
+        // println!("-> {} max: {}", buka, max);
+    }
+    dp[buka] = max + 1;
+    // println!("--> dp[{}]: {}", buka, dp[buka]);
+    return dp[buka];
+}
+
 fn main() {
-    input! {}
+    input! {(n, t): (usize, usize)}
+    let mut g = vec![vec![]; n + 1];
+    for _ in 0..n - 1 {
+        input! {(a, b): (usize, usize)}
+        g[a].push(b);
+        g[b].push(a);
+    }
+
+    let mut dp = vec![0; n + 1];
+    dfs(&g, &mut dp, t, t);
+    dp.remove(0);
+    println!("{}", dp.iter().join(" "));
 }
