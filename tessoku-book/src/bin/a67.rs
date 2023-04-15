2731eaa7bb2e9102dc59cd2bd41261b37c871e4b
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2023-04-15 20:58:55 +0900

    追加：4/15分

--- tessoku-book/src/bin/a67.rs
+++ tessoku-book/src/bin/a67.rs
@@ -1,4 +1,21 @@
+use petgraph::unionfind::UnionFind;
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        (n, m): (usize, usize),
+        mut abc: [(usize, usize, usize); m]
+    }
+    abc.sort_unstable_by(|(_, _, c1), (_, _, c2)| c1.cmp(c2));
+
+    let mut uf = UnionFind::<usize>::new(n);
+    let mut ans = 0usize;
+    for (a, b, c) in abc {
+        if uf.equiv(a - 1, b - 1) {
+            continue;
+        } else {
+            uf.union(a - 1, b - 1);
+            ans += c;
+        }
+    }
+    println!("{}", ans);
 }
--- tessoku-book/src/bin/b67.rs
+++ tessoku-book/src/bin/b67.rs
@@ -1,4 +1,19 @@
+use petgraph::unionfind::UnionFind;
 use proconio::input;
 fn main() {
-    input! {}
+    input! {
+        (n, m): (usize, usize),
+        mut abc: [(usize, usize, usize); m]
+    }
+    abc.sort_unstable_by(|(_, _, c1), (_, _, c2)| c2.cmp(c1));
+
+    let mut uf = UnionFind::new(n);
+    let mut ans = 0;
+    for (a, b, c) in abc {
+        if !uf.equiv(a - 1, b - 1) {
+            uf.union(a - 1, b - 1);
+            ans += c;
+        }
+    }
+    println!("{}", ans);
 }
