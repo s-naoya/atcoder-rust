2257a2a42f6afbd396d74d4bda88c24e6d3d6753
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2023-05-09 22:13:01 +0900

    追加：5/8分

--- bootcamp-easy/src/bin/38.rs
+++ bootcamp-easy/src/bin/38.rs
@@ -1,4 +1,36 @@
+use itertools::Itertools;
 use proconio::input;
 fn main() {
-    input! {}
+    ans2();
+}
+// fn ans1() {
+//     input! {abcde: [usize; 5]}
+//     let mut ans = 1_000_000;
+//     for a in abcde.iter().permutations(5) {
+//         let mut sum = 0;
+//         for &b in a {
+//             sum += if sum % 10 != 0 { 10 - sum % 10 } else { 0 };
+//             sum += b;
+//         }
+//         ans = ans.min(sum);
+//     }
+//     println!("{}", ans);
+// }
+// // 23:22
+fn ans2() {
+    input! {abcde: [usize; 5]}
+    let f = |acc: usize, x: &&usize| {
+        if acc % 10 != 0 {
+            acc + 10 - acc % 10 + *x
+        } else {
+            acc + *x
+        }
+    };
+    let ans = abcde
+        .iter()
+        .permutations(5)
+        .map(|x| x.iter().fold(0, f))
+        .min()
+        .unwrap();
+    println!("{}", ans);
 }
