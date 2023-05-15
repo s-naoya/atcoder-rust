f577aa9b90245d7b073565947d34ad48ba21667c
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2023-05-16 06:57:57 +0900

    追加：5/16文

--- bootcamp-easy/src/bin/47.rs
+++ bootcamp-easy/src/bin/47.rs
@@ -1,4 +1,24 @@
+use std::collections::HashMap;
+
 use proconio::input;
 fn main() {
-    input! {}
+    ans1();
 }
+fn ans1() {
+    input! {n: u128}
+    let mut memo = HashMap::new();
+    memo.insert(0, 2);
+    memo.insert(1, 1);
+    println!("{}", l(n, &mut memo));
+}
+fn l(i: u128, memo: &mut HashMap<u128, u128>) -> u128 {
+    match memo.contains_key(&i) {
+        true => *memo.get(&i).unwrap(),
+        false => {
+            let a = l(i - 1, memo) + l(i - 2, memo);
+            memo.insert(i, a);
+            a
+        }
+    }
+}
+// 6:42
