3eafba178fd4355830317814fb66a4beec22f3fa
Saito Naoya <6813222+s-naoya@users.noreply.github.com>
2023-05-20 20:57:55 +0900

    追加：5/20分

--- bootcamp-easy/src/bin/55.rs
+++ bootcamp-easy/src/bin/55.rs
@@ -1,4 +1,24 @@
 use proconio::input;
 fn main() {
-    input! {}
+    ans1();
+}
+fn ans1() {
+    input! {
+        (n, d): (usize, usize),
+        x: [[f64; d]; n]
+    }
+    let mut ans = 0;
+    for i in 0..n {
+        for j in i + 1..n {
+            let mut z = 0.0;
+            for k in 0..d {
+                z += (x[i][k] - x[j][k]) * (x[i][k] - x[j][k]);
+            }
+            z = z.sqrt();
+            if z == z.floor() {
+                ans += 1;
+            }
+        }
+    }
+    println!("{}", ans);
 }
